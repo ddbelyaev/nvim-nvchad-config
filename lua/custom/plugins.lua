1111105f0292014e1598dd34bc7dd34f7a2dc393
Dmitry Romanidis <ddromanidis@Dmitrys-MacBook-Pro.local>
2025-09-18 00:11:04 +0300

    updated commands; added dadbod

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -228,5 +228,22 @@ local plugins = {
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
 return plugins
